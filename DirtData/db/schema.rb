b2c09d4d2debc9d5834bf1ca8741c48299b13a35
Vincent Lopez <vincent-lopez@uiowa.edu>
2018-03-19 20:04:23 +0000

    garden/user

--- DirtData/db/schema.rb
+++ DirtData/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207210219) do
+ActiveRecord::Schema.define(version: 20180319195602) do
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "readings", force: :cascade do |t|
     t.datetime "time"
