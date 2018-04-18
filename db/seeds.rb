e0895c3f743e98f2bebe06e128056f51febebe80
Vincent Lopez <vincent-lopez@uiowa.edu>
2018-04-18 02:45:41 +0000

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gardens = [{:name => 'UIOWA Garden', :password => 'TESTPASSWORD'}]
+
+gardens.each do |garden|
+    Garden.create!(garden)
+end
+water_readings = [{:node1 => 50, :node2 => 50, :node3 => 50, :node4 => 50, :node5 => 50, :node6 => 50 , :node7 => 50, :node8 => 50, :garden_id => 1}]
+water_readings.each do |reading|
+    WaterReading.create!(reading)
+end
+light_readings = [{:node1 => 50, :node2 => 50, :node3 => 50, :node4 => 50, :node5 => 50, :node6 => 50 , :node7 => 50, :node8 => 50, :temp => 50, :garden_id => 1}]
+light_readings.each do |reading|
+    LightReading.create!(reading)
+end
