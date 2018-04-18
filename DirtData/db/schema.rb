e1af53694565ef02f8f6cd09b6bcc09bec79f641
Vincent Lopez <vincent-lopez@uiowa.edu>
2018-04-18 03:50:33 +0000

    working on local again

--- DirtData/db/schema.rb
+++ DirtData/db/schema.rb
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 20180409232502) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "readings", force: :cascade do |t|
-    t.datetime "time"
-    t.string "moisture"
-    t.string "light"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
