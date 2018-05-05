d0144cf9f6cc6d322aa97653642297c126dee21e
harrylf <harrylf@ymail.com>
2018-05-04 20:21:58 -0500

    Add files via upload

---
+++ RadioReciever.ino
@@ -0,0 +1,195 @@
+#include <RadioHead.h>
+#include <radio_config_Si4460.h>
+#include <RHCRC.h>
+#include <RHDatagram.h>
+#include <RHGenericDriver.h>
+#include <RHGenericSPI.h>
+#include <RHHardwareSPI.h>
+#include <RHMesh.h>
+#include <RHNRFSPIDriver.h>
+#include <RHReliableDatagram.h>
+#include <RHRouter.h>
+#include <RHSoftwareSPI.h>
+#include <RHSPIDriver.h>
+#include <RHTcpProtocol.h>
+#include <RH_ASK.h>
+#include <RH_NRF24.h>
+#include <RH_NRF905.h>
+#include <RH_RF22.h>
+#include <RH_RF24.h>
+#include <RH_RF69.h>
+#include <RH_RF95.h>
+#include <RH_Serial.h>
+#include <RH_TCP.h>
+
+#include <RadioHead.h>
+#include <radio_config_Si4460.h>
+#include <RHCRC.h>
+#include <RHDatagram.h>
+#include <RHGenericDriver.h>
+#include <RHGenericSPI.h>
+#include <RHHardwareSPI.h>
+#include <RHMesh.h>
+#include <RHNRFSPIDriver.h>
+#include <RHReliableDatagram.h>
+#include <RHRouter.h>
+#include <RHSoftwareSPI.h>
+#include <RHSPIDriver.h>
+#include <RHTcpProtocol.h>
+#include <RH_ASK.h>
+#include <RH_NRF24.h>
+#include <RH_NRF905.h>
+#include <RH_RF22.h>
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK driver;
+char node1w[4] = "000";
+char node1l[4] = "000";
+char node2w[4] = "000";
+char node2l[4] = "000";
+char node3w[4] = "000";
+char node3l[4] = "000";
+char node4w[4] = "000";
+char node4l[4] = "000";
+char node5w[4] = "000";
+char node5l[4] = "000";
+char node6w[4] = "000";
+char node6l[4] = "000";
+char node7w[4] = "000";
+char node7l[4] = "000";
+char node8w[4] = "000";
+char node8l[4] = "000";
+int readcount = 0;
+void setup()
+{
+  
+    Serial.begin(9600); // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    uint8_t buf[9];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+      int i;
+      String str = (char*)buf;
+      // Message with a good checksum received, dump it.
+      char node = str[8];
+      switch(node){
+        case '1':
+          node1w[0] = str[0];
+          node1w[1] = str[1];
+          node1w[2] = str[2];
+          node1l[0] = str[4];
+          node1l[1] = str[5];
+          node1l[2] = str[6];
+        break;
+        case '2':
+          node2w[0] = str[0];
+          node2w[1] = str[1];
+          node2w[2] = str[2];
+          node2l[0] = str[4];
+          node2l[1] = str[5];
+          node2l[2] = str[6];
+          Serial.println(node2w);
+          Serial.println(node2l);
+        break;
+        case '3':
+          node3w[0] = str[0];
+          node3w[1] = str[1];
+          node3w[2] = str[2];
+          node3l[0] = str[4];
+          node3l[1] = str[5];
+          node3l[2] = str[6];
+          Serial.println(node3w);
+          Serial.println(node3l);
+        break;
+        case '4':
+          node4w[0] = str[0];
+          node4w[1] = str[1];
+          node4w[2] = str[2];
+          node4l[0] = str[4];
+          node4l[1] = str[5];
+          node4l[2] = str[6];
+        break;
+        case '5':
+          node5w[0] = str[0];
+          node5w[1] = str[1];
+          node5w[2] = str[2];
+          node5l[0] = str[4];
+          node5l[1] = str[5];
+          node5l[2] = str[6];
+        break;
+        case '6':
+          node6w[0] = str[0];
+          node6w[1] = str[1];
+          node6w[2] = str[2];
+          node6l[0] = str[4];
+          node6l[1] = str[5];
+          node6l[2] = str[6];
+        break;
+        case '7':
+          node7w[0] = str[0];
+          node7w[1] = str[1];
+          node7w[2] = str[2];
+          node7l[0] = str[4];
+          node7l[1] = str[5];
+          node7l[2] = str[6];
+        break;
+        case '8':
+          node8w[0] = str[0];
+          node8w[1] = str[1];
+          node8w[2] = str[2];
+          node8l[0] = str[4];
+          node8l[1] = str[5];
+          node8l[2] = str[6];
+        break;
+      }
+      readcount++;
+      if(readcount == 8){
+        readcount = 0;
+        char waterSendMessage[34];
+        char lightSendMessage[34];
+        strcpy(waterSendMessage,"1,");
+        strcpy(lightSendMessage,"0,");
+        strcat(waterSendMessage,node1w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node1l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node2w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node2l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node3w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node3l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node4w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node4l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node5w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node5l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node6w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node6l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node7w);
+        strcat(waterSendMessage,",");
+        strcat(lightSendMessage,node7l);
+        strcat(lightSendMessage,",");
+        strcat(waterSendMessage,node8w);
+        strcat(lightSendMessage,node8l);
+        Serial.println(waterSendMessage);
+        delay(2000);
+        Serial.println(lightSendMessage);
+      }
+    }
+}
---
+++ RadioTransmitterUpdato.ino
@@ -0,0 +1,111 @@
+  #include <RadioHead.h>
+#include <radio_config_Si4460.h>
+#include <RHCRC.h>
+#include <RHDatagram.h>
+#include <RHGenericDriver.h>
+#include <RHGenericSPI.h>
+#include <RHHardwareSPI.h>  
+#include <RHMesh.h>
+#include <RHNRFSPIDriver.h>
+#include <RHReliableDatagram.h>
+#include <RHRouter.h>
+#include <RHSoftwareSPI.h>
+#include <RHSPIDriver.h>
+#include <RHTcpProtocol.h>
+#include <RH_ASK.h>
+#include <RH_NRF24.h>
+#include <RH_NRF905.h>
+#include <RH_RF22.h>
+#include <RH_RF24.h>
+#include <RH_RF69.h>
+#include <RH_RF95.h>
+#include <RH_Serial.h>
+#include <RH_TCP.h>
+#include <LowPower.h>
+
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+int RESETPin = PD5;
+int analogPin = A0; //pin A0 or #4
+int lightPin = A1;
+int i = 1;
+int moisture; //raw value
+int percentage; //percentage
+int lpercentage;
+int light;
+int map_low = 706;  //These values relate to a 3.3 Vin OG 706
+int map_high = 350;  //OG: 350
+int light_low = 895;
+int light_high = 230;
+int brightness;
+void(*resetFunc) (void) = 0;
+void setup()
+{
+    Serial.begin(9600);   // Debugging only
+    Serial.println("3");
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    delay(1000);
+    unsigned int sleep;
+      for(sleep = 150; sleep >0;sleep--){
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    }
+    delay(1000);
+    char water[4] = "";
+    char bright[4] = "";
+    char transmission[10] = "";
+    driver.waitPacketSent();
+    int percent = 0;
+    moisture = 0;
+    String percentagelstr;
+    String percentagewstr;
+    char wpercent[4];
+    char lpercent[4];
+
+    moisture = analogRead(analogPin);
+    Serial.print("moisture:");
+    Serial.println(moisture);
+    light = analogRead(lightPin);
+    Serial.print("light");
+    Serial.println(light);
+    
+
+
+    percentage = map(moisture, map_low, map_high, 0, 100);
+    lpercentage = map(light, light_low, light_high, 0, 100);
+    if(percentage > 100)
+      percentage = 100;
+    if(percentage <0)
+      percentage = 0;
+    if(lpercentage > 100)
+      lpercentage = 100;
+    if(lpercentage <0)
+      lpercentage = 0;
+    percentagewstr = String(percentage);     
+    percentagelstr = String(lpercentage);
+    percentagewstr.toCharArray(wpercent, 4);
+    percentagelstr.toCharArray(lpercent, 4);
+    if(percentage < 100)
+      strcat(transmission,"0");
+    if(percentage < 10)
+      strcat(transmission,"0");  
+    strcat(transmission,wpercent);    
+    strcat(transmission, ",");
+    
+    if(lpercentage < 100)
+      strcat(transmission,"0");
+    if(lpercentage < 10)
+      strcat(transmission,"0");
+    strcat(transmission, lpercent);  
+    strcat(transmission, ",");
+    strcat(transmission, "6");
+    driver.send((const char *)transmission, strlen(transmission));
+    Serial.println(transmission);
+}
